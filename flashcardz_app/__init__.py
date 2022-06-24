a7d665d12828955e172e8d8d55dea8f682161579
Jonathan Trull <jtrull@134599-MBP.hsd1.co.comcast.net>
2022-06-23 19:41:39 -0600

    added static and template folders

--- flashcardz_app/__init__.py
+++ flashcardz_app/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
